37916bde6cecbf96d57adcea021f62582c65a804
Leo Linbeck III <leo3@Leos-MacBook-Air.local>
2013-04-05 22:34:05 -0500

    Initial splash screen

---
+++ CommonApp/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var buttonCAStart = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	buttonCAStart.click = function buttonCAStart_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("buttonCAStart", "click", buttonCAStart.click, "WAF");
+// @endregion
+};// @endlock
