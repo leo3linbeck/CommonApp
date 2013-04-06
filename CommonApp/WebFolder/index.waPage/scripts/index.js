b15e523badde68b30deab07b355bb595eb1cdfe2
Leo Linbeck III <leo3@Leos-MacBook-Air.local>
2013-04-06 12:21:31 -0500

    Add buttons and simple functionality

--- CommonApp/WebFolder/index.waPage/scripts/index.js
+++ CommonApp/WebFolder/index.waPage/scripts/index.js
@@ -2,17 +2,41 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var buttonCAStart = {};	// @button
+	var buttonNextStep = {};	// @button
+	var buttonGoBack = {};	// @button
+	var buttonStart = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
-	buttonCAStart.click = function buttonCAStart_click (event)// @startlock
+	buttonNextStep.click = function buttonNextStep_click (event)// @startlock
 	{// @endlock
-		// Add your code here
+		$$('componentAddressEntry').hide();
+	};// @lock
+
+	buttonGoBack.click = function buttonGoBack_click (event)// @startlock
+	{// @endlock
+		$$('buttonGoBack').hide();
+		$$('buttonNextStep').hide();
+		$$('componentAddressEntry').hide();
+		
+		$$('buttonStart').show();
+		$$('richTextSplashDescription').show();
+	};// @lock
+
+	buttonStart.click = function buttonStart_click (event)// @startlock
+	{// @endlock
+		$$('buttonStart').hide();
+		$$('richTextSplashDescription').hide();
+		
+		$$('buttonGoBack').show();
+		$$('buttonNextStep').show();
+		$$('componentAddressEntry').show();
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("buttonCAStart", "click", buttonCAStart.click, "WAF");
+	WAF.addListener("buttonNextStep", "click", buttonNextStep.click, "WAF");
+	WAF.addListener("buttonGoBack", "click", buttonGoBack.click, "WAF");
+	WAF.addListener("buttonStart", "click", buttonStart.click, "WAF");
 // @endregion
 };// @endlock
