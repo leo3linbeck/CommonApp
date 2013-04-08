cfdcc0ba0eca5f73dff0d8c54c8baa3958c278cf
Leo Linbeck III <leo3@linbeck.com>
2013-04-07 23:11:20 -0500

    Address verification functional

--- CommonApp/WebFolder/Components/AddressEntryAndVerify.waComponent/AddressEntryAndVerify.js
+++ CommonApp/WebFolder/Components/AddressEntryAndVerify.waComponent/AddressEntryAndVerify.js
@@ -15,14 +15,35 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var textFieldCityEntry = {};	// @textField
+	var textFieldStreet2Entry = {};	// @textField
+	var textFieldStreet1Entry = {};	// @textField
 	var buttonVerifyAddress = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	textFieldCityEntry.change = function textFieldCityEntry_change (event)// @startlock
+	{// @endlock
+		homeCity = L3.toTitleCase(this.getValue());
+		source.homeCity.sync();
+	};// @lock
+
+	textFieldStreet2Entry.change = function textFieldStreet2Entry_change (event)// @startlock
+	{// @endlock
+		homeAddress2 = L3.toTitleCase(this.getValue());
+		source.homeAddress2.sync();
+	};// @lock
+
+	textFieldStreet1Entry.change = function textFieldStreet1Entry_change (event)// @startlock
+	{// @endlock
+		homeAddress1 = L3.toTitleCase(this.getValue());
+		source.homeAddress1.sync();
+	};// @lock
+
 	buttonVerifyAddress.click = function buttonVerifyAddress_click (event)// @startlock
 	{// @endlock
-		CommonApp.verifyAddress({
+		CommonApp.verifyAddressAsync({
 			onSuccess: function(response) {
 				if (response) {
 					if (!response.success) {
@@ -30,7 +51,7 @@ function constructor (id) {
 						$$(getHtmlId('buttonVerifyAddress')).enable();
 					}
 					else {
-						$$(getHtmlId('textFieldZipEntry')).setValue(response.zip9);
+						$$(getHtmlId('textFieldZipEntry')).setValue(response.fullAddress.components.zipcode + '-' + response.fullAddress.components.plus4_code);
 						$$(getHtmlId('richTextUSPSLine1')).setValue(response.USPS[0]);
 						$$(getHtmlId('richTextUSPSLine2')).setValue(response.USPS[1]);
 						$$(getHtmlId('buttonVerifyAddress')).disable();
@@ -45,11 +66,17 @@ function constructor (id) {
 				alert('Validation error');
 				$$(getHtmlId('buttonVerifyAddress')).enable();
 			},
-			params: [ {}, true ]
+			params: [ {	addr1: $$(getHtmlId('textFieldStreet1Entry')).getValue(),
+						addr2: $$(getHtmlId('textFieldStreet2Entry')).getValue(),
+						city: $$(getHtmlId('textFieldCityEntry')).getValue(),
+						zip: $$(getHtmlId('textFieldZipEntry')).getValue() }, false ]
 		});
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_textFieldCityEntry", "change", textFieldCityEntry.change, "WAF");
+	WAF.addListener(this.id + "_textFieldStreet2Entry", "change", textFieldStreet2Entry.change, "WAF");
+	WAF.addListener(this.id + "_textFieldStreet1Entry", "change", textFieldStreet1Entry.change, "WAF");
 	WAF.addListener(this.id + "_buttonVerifyAddress", "click", buttonVerifyAddress.click, "WAF");
 	// @endregion// @endlock
 
