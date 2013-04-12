dede6f941fd62611080a72d617c852c6023deeba
Leo Linbeck III <leo3@linbeck.com>
2013-04-12 01:24:40 -0500

    New Model and more GoogleMap fun

--- CommonApp/Model.js
+++ CommonApp/Model.js
@@ -1,1 +1,103 @@
-﻿model = {};
+﻿model = new DataStoreCatalog();
+
+var NEW_FAMILY_TEXT = 'SELECT TO ADD NEW FAMILY';
+
+var fam = model.addClass('Family','Families');
+fam.addAttribute('ID', 'storage','uuid', 'key auto');
+fam.addAttribute('name', 'storage','string', 'btree');
+fam.addAttribute('homeStreet1', 'storage','string', 'btree');
+fam.addAttribute('homeStreet2', 'storage','string', 'btree');
+fam.addAttribute('homeCity', 'storage','string', 'cluster');
+fam.addAttribute('homeState', 'storage','string', 'cluster');
+fam.addAttribute('homeZipCode', 'storage','string', 'btree');
+fam.addAttribute('homePhone', 'storage','string', 'btree');
+fam.addAttribute('workPhone', 'storage','string', 'btree');
+fam.addAttribute('cellPhone', 'storage','string', 'btree');
+fam.addAttribute('uspsLine1', 'storage','string', 'btree');
+fam.addAttribute('uspsLine2', 'storage','string', 'btree');
+fam.addAttribute('uspsDeliveryPoint', 'storage','string', 'btree');
+fam.addAttribute('race', 'storage','string', 'cluster');
+fam.addAttribute('fplStatus', 'storage','string', 'btree');
+fam.addAttribute('mapCoords', 'storage','string', 'btree');
+fam.addAttribute('nearbySchools', 'relatedEntities','FamilySchools', 'family', {reversePath: true});
+fam.addMethod('addressLookup', 'dataClass',
+	function addressLookup(param) {
+		var xhr, getString, v, c, families;
+
+		if (param.debug) {
+			debugger;
+		}
+		
+		try {
+			xhr = new XMLHttpRequest(); 
+			getString = 'https://api.smartystreets.com/street-address?street=' + param.street1 + (param.street2 ? '&street2=' + param.street2 : '') + '&city=' + param.city + '&state=TX&zipcode=' + param.zipCode + '&candidates=5&auth-id=fd972f01-719a-4d50-88e6-b0ce3f936098&auth-token=sEv4KscGrL2PJMD6WKCoxWpR%2BhsXCj6ht%2F9aU4VfpebYfRKwSO8rO%2BwkPohcjMyLpu6ujyDryvou5dqMZvSLzg%3D%3D';
+			xhr.open('GET', getString.replace(/ /g,'+'), false); // to connect to a Web site synchronously
+			xhr.send(); // send the request
+		} catch (e) {
+			return null;
+		}
+
+		if (xhr.status === 200) {
+			v = JSON.parse(xhr.responseText)[0];
+			
+			families = ds.Family.query('uspsDeliveryPoint = :1', v.delivery_point_barcode);
+			
+			c = families.find('name == :1', NEW_FAMILY_TEXT);
+			if (c === null) {
+				c = ds.Family.createEntity();
+				c.name = NEW_FAMILY_TEXT;
+				c.uspsDeliveryPoint = v.delivery_point_barcode;
+				families.add(c);
+			}
+			c.homeStreet1 = param.street1;
+			c.homeStreet2 = param.street2;
+			c.homeCity = param.city;
+			c.homeState = v.components.state_abbreviation;
+			c.homeZipCode = v.components.zipcode + '-' + v.components.plus4_code;
+			c.uspsLine1 = v.delivery_line_1;
+			c.uspsLine2 = v.last_line;
+			c.mapCoords = v.metadata.latitude + ',' + v.metadata.longitude;
+			c.save();
+			
+			return families;
+		}
+		else {
+			return null;
+		}
+	},
+'public');
+
+var smo = model.addClass('SMO','SMOs');
+smo.addAttribute('ID', 'storage','uuid', 'key auto');
+smo.addAttribute('name', 'storage','string', 'btree');
+smo.addAttribute('category', 'storage','string', 'cluster');
+smo.addAttribute('superintendent', 'storage','string', 'btree');
+smo.addAttribute('streetAddress1', 'storage','string', 'btree');
+smo.addAttribute('streetAddress2', 'storage','string', 'btree');
+smo.addAttribute('city', 'storage','string', 'cluster');
+smo.addAttribute('state', 'storage','string', 'cluster');
+smo.addAttribute('zipCode', 'storage','string', 'btree');
+smo.addAttribute('uspsDeliveryPoint', 'storage','string', 'btree');
+smo.addAttribute('schools', 'relatedEntities','Schools', 'smo', {reversePath: true});
+
+var sch = model.addClass('School','Schools');
+sch.addAttribute('ID', 'storage','uuid', 'key auto');
+sch.addAttribute('name', 'storage','string', 'btree');
+sch.addAttribute('category', 'storage','string', 'cluster');
+sch.addAttribute('schoolLeader', 'storage','string', 'btree');
+sch.addAttribute('startingGrade', 'storage','long', 'btree');
+sch.addAttribute('endingGrade', 'storage','long', 'btree');
+sch.addAttribute('streetAddress1', 'storage','string', 'btree');
+sch.addAttribute('streetAddress2', 'storage','string', 'btree');
+sch.addAttribute('city', 'storage','string', 'cluster');
+sch.addAttribute('state', 'storage','string', 'cluster');
+sch.addAttribute('zipCode', 'storage','string', 'btree');
+sch.addAttribute('uspsDeliveryPoint', 'storage','string', 'btree');
+sch.addAttribute('smo', 'relatedEntity','SMO', 'SMO');
+
+var famSch = model.addClass('FamilySchool','FamilySchools');
+famSch.addAttribute('ID', 'storage','uuid', 'key auto');
+famSch.addAttribute('family', 'relatedEntity','Family', 'Family');
+famSch.addAttribute('school', 'relatedEntity','School', 'School');
+famSch.addAttribute('distance', 'storage','number', 'btree');
+
--- CommonApp/WebFolder/Components/AddressEntryAndVerify.waComponent/AddressEntryAndVerify.js
+++ CommonApp/WebFolder/Components/AddressEntryAndVerify.waComponent/AddressEntryAndVerify.js
@@ -15,6 +15,7 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var buttonSelectFamily = {};	// @button
 	var textFieldCityEntry = {};	// @textField
 	var textFieldStreet2Entry = {};	// @textField
 	var textFieldStreet1Entry = {};	// @textField
@@ -22,6 +23,12 @@ function constructor (id) {
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
+
+	buttonSelectFamily.click = function buttonSelectFamily_click (event)// @startlock
+	{// @endlock
+		sources.family.name = '';
+		$$(getHtmlId("dialogSelectFamily")).closeDialog(); //ok button
+	};// @lock
 
 	textFieldCityEntry.change = function textFieldCityEntry_change (event)// @startlock
 	{// @endlock
@@ -37,49 +44,42 @@ function constructor (id) {
 	{// @endlock
 		L3.convertToTitleCase(this);
 	};// @lock
+	
 
 	buttonVerifyAddress.click = function buttonVerifyAddress_click (event)// @startlock
 	{// @endlock
-		CommonApp.verifyAddressAsync({
-			onSuccess: function(response) {
-				var a;
-				
-				if (response) {
-					if (!response.success) {
-						alert('Address not found');
-						L3.currentAddress = null;
+		sources.family.addressLookup(
+			{	street1: $$(getHtmlId('textFieldStreet1Entry')).getValue(),
+				street2: $$(getHtmlId('textFieldStreet2Entry')).getValue(),
+				city: $$(getHtmlId('textFieldCityEntry')).getValue(),
+				zip: $$(getHtmlId('textFieldZipEntry')).getValue(),
+				debug: false 
+			},
+			{
+				onSuccess: function(response) {
+					var r = response.result;
+
+					if (response.result) {
+						sources.family.setEntityCollection(response.result);
+						sources.family.autoDispatch();
+						$$(getHtmlId('dialogSelectFamily')).displayDialog();
+						$$(getHtmlId('textFieldZipEntry')).setValue(sources.family.homeZipCode);
+						$$(getHtmlId('richTextUSPSLine1')).setValue(sources.family.uspsLine1);
+						$$(getHtmlId('richTextUSPSLine2')).setValue(sources.family.uspsLine2);
 					}
 					else {
-						a = response.fullAddress;
-						source.family.mapCoords = a.metadata.latitude + ',' + a.metadata.longitude;
-						
-						$$(getHtmlId('textFieldZipEntry')).setValue(response.fullAddress.components.zipcode + '-' + response.fullAddress.components.plus4_code);
-						$$(getHtmlId('richTextUSPSLine1')).setValue(response.USPS[0]);
-						$$(getHtmlId('richTextUSPSLine2')).setValue(response.USPS[1]);
+						$$(getHtmlId('richTextVerifyAddressError')).setValue('Validation failed!');
 					}
+				},
+				onError: function(error) {
+					$$(getHtmlId('richTextVerifyAddressError')).setValue(error.message);
 				}
-				else {
-					alert('Validation failed');
-					L3.currentAddress = null;
-				}
-//				googleMapString = L3.currentAddress.metadata.latitude + ',' + L3.currentAddress.metadata.latitude;
-//				source.googleMapString.sync();
-//				$$('componentSchoolMap_googleMapsBuildSchoolList').setCenter(googleMapString);
-//				$$('componentSchoolMap_googleMapsBuildSchoolList').setMarker(sources.googleMapString.getID(), googleMapString, {title: 'Your Home'});
-		//		$$(getHtmlId('googleMapsBuildSchoolList')).setZoom(15);
-			},
-			onError: function(error) {
-				alert('Validation error');
-				L3.currentAddress = null;
-			},
-			params: [ {	addr1: $$(getHtmlId('textFieldStreet1Entry')).getValue(),
-						addr2: $$(getHtmlId('textFieldStreet2Entry')).getValue(),
-						city: $$(getHtmlId('textFieldCityEntry')).getValue(),
-						zip: $$(getHtmlId('textFieldZipEntry')).getValue() }, false ]
-		});
+			}
+		);
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_buttonSelectFamily", "click", buttonSelectFamily.click, "WAF");
 	WAF.addListener(this.id + "_textFieldCityEntry", "change", textFieldCityEntry.change, "WAF");
 	WAF.addListener(this.id + "_textFieldStreet2Entry", "change", textFieldStreet2Entry.change, "WAF");
 	WAF.addListener(this.id + "_textFieldStreet1Entry", "change", textFieldStreet1Entry.change, "WAF");
